c2c15d44f455d42ae2832a8a160af885af3b054b
casjay <git-admin@casjaysdev.com>
2022-08-30 19:13:05 -0400

    🦈🏠🐜❗ Initial Commit ❗🐜🦈🏠

---
+++ README.md
@@ -0,0 +1,42 @@
+# Welcome to URL Shortner 👋
+
+![Version](https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000)
+[![Documentation](https://img.shields.io/badge/documentation-yes-brightgreen.svg)](https://github.com/now-sh/url)
+[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-yellow.svg)](#)
+[![Twitter: casjaysdev](https://img.shields.io/twitter/follow/casjaysdev.svg?style=social)](https://twitter.com/casjaysdev)
+
+
+## Install  
+  
+Copy the env.sample file to .env and edit  
+  
+
+## Usage for Development  
+
+```sh
+npm i -D
+npm run dev
+```
+
+## Usage for production  
+
+```sh
+npm install
+npm run start
+```
+
+### 🏠 [Homepage](https://github.com/now-sh/gitsh)
+
+### ✨ [Demo](https://gitsh.vercel.app)
+
+#### Author
+
+👤 **CasjaysDev**
+
+* Website: <http://malaks-us.github.io/jason>
+* Twitter: [@casjaysdev](https://twitter.com/casjaysdev)
+* Github: [@casjay](https://github.com/casjay)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
---
+++ index.js
@@ -0,0 +1,130 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk');
+const rateLimit = require('express-rate-limit');
+const slowDown = require('express-slow-down');
+const cors = require('cors');
+const { nanoid } = require('nanoid');
+
+require('dotenv').config({ path: './.env' });
+
+const port = process.env.PORT || '1337';
+const urlHost = process.env.URL_HOST || 'localhost';
+const node_Mode = process.env.NODE_ENV || 'development';
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/url';
+
+const db = monk(mongoURI);
+const urls = db.get('urls');
+urls.createIndex(
+  {
+    slug: 1,
+  },
+  {
+    unique: true,
+  }
+);
+
+const app = express();
+app.enable('trust proxy');
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')));
+
+const publicFolder = path.join(__dirname, 'public');
+const notFoundPath = path.join(__dirname, 'public/404.html');
+
+app.get('/:id', async (req, res, next) => {
+  const { id: slug } = req.params;
+  try {
+    const url = await urls.findOne({
+      slug,
+    });
+    if (url) {
+      return res.redirect(url.url);
+    }
+    return res.status(404).sendFile(notFoundPath);
+  } catch (error) {
+    return res.status(404).sendFile(notFoundPath);
+  }
+});
+
+const schema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    .matches(/^[\w\-]+$/i),
+  url: yup.string().trim().url().required(),
+});
+
+app.post(
+  '/url',
+  slowDown({
+    windowMs: 10 * 1000,
+    delayAfter: 1,
+    delayMs: 500,
+  }),
+  rateLimit({
+    windowMs: 10 * 100,
+    max: 3,
+  }),
+  async (req, res, next) => {
+    let { slug, url } = req.body;
+    try {
+      await schema.validate({
+        slug,
+        url,
+      });
+      if (url.includes(urlHost)) {
+        throw new Error('Stop it. 🛑');
+      }
+      if (!slug) {
+        slug = nanoid(5);
+      } else {
+        const existing = await urls.findOne({
+          slug,
+        });
+        if (existing) {
+          throw new Error('Slug in use. 🍔');
+        }
+      }
+      slug = slug.toLowerCase();
+      const newUrl = {
+        url,
+        slug,
+      };
+      const created = await urls.insert(newUrl);
+      res.json(created);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(notFoundPath);
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+  });
+});
+
+app.get('/', express.static(publicFolder));
+
+app.listen(port, () => {
+  console.log(`Listening on ${port} in ${node_Mode} mode`);
+});
